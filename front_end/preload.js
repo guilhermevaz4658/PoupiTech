c2d7a79fba52b56d7ccff0e2c5c727b730450293
Jessica <jessicafagundesnunes@gmail.com>
2026-03-25 16:47:38 -0300

    feat: adicionando pasta Front_end

--- front_end/preload.js
+++ front_end/preload.js
@@ -1,22 +1,17 @@
 const { contextBridge } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-
- 
   salvarTransacao: async (dados) => {
-    const response = await fetch('http://localhost:3000/transacoes', {
+    const res = await fetch('http://localhost:3000/transacoes', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(dados)
     });
-
-    return response.json();
+    return res.json();
   },
 
- 
   listarTransacoes: async (usuario_id) => {
-    const response = await fetch(`http://localhost:3000/transacoes/${usuario_id}`);
-    return response.json();
+    const res = await fetch(`http://localhost:3000/transacoes/${usuario_id}`);
+    return res.json();
   }
-
 });
