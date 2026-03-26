b188fb0921e587dde7b6fa94f0f013f51b96ba56
Jessica <jessicafagundesnunes@gmail.com>
2026-03-26 17:05:37 -0300

    feat: adiciona arquivos front_end

---
+++ front_end/tela_login/script.js
@@ -0,0 +1,26 @@
+async function login() {
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    const resposta = await fetch('http://localhost:3000/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, senha })
+    });
+
+    const dados = await resposta.json();
+
+    if (resposta.ok) {
+        localStorage.setItem('token', dados.token);
+
+        // redireciona pra tela principal
+        window.location.href = '../tela_principal/index.html';
+    } else {
+        document.getElementById('mensagem').innerText = dados.erro;
+    }
+}
+
+function irParaCadastro() {
+    // você pode criar essa tela depois
+    window.location.href = '../tela_cadastro/index.html';
+}
